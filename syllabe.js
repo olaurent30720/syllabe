698f73594706f01a993ff90491be9aa536e1207e
olaurent30720 <73831381+olaurent30720@users.noreply.github.com>
2020-12-04 16:05:17 +0100

    Add files via upload

---
+++ syllabe.js
@@ -0,0 +1,20 @@
+let a = document.querySelector('#rep3');
+console.log('bou');
+a.addEventListener('click',function(){
+let bruit = new Audio("https://lasonotheque.org/UPLOAD/mp3/0237.mp3");
+bruit.play();
+});
+
+let b = document.querySelector('#rep2');
+console.log('bou');
+b.addEventListener('click',function(){
+let bruit = new Audio("https://lasonotheque.org/UPLOAD/mp3/1554.mp3");
+bruit.play();
+});
+
+let c = document.querySelector('#rep1');
+console.log('bou');
+c.addEventListener('click',function(){
+let bruit = new Audio("https://lasonotheque.org/UPLOAD/mp3/1554.mp3");
+bruit.play();
+});
